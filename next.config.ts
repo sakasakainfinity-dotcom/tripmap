2601c36ded3cf8dd4a11f4721533247504af5a44
sakasakainfinity-dotcom <sakasaka_infinity@icloud.com>
2025-09-23 08:52:59 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,4 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "*.maptiler.com",
-      },
-    ],
-  },
-};
-
+const nextConfig: NextConfig = {};
 export default nextConfig;
+
